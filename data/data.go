38e8e3b66b87b7de76e23b3502333488e97da1be
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-18 20:12:29 +0700

    Add data var

--- data/data.go
+++ data/data.go
@@ -2,14 +2,10 @@ package data
 
 import "github.com/MarioTiara/Go-API-Gin/model"
 
-type Books struct {
-	item []model.Book
-}
-
-func (b *Books) GetBooks() []model.Book {
-	return b.item
-}
-
-func (b *Books) AddBooks(input model.Book) {
-	b.item = append(b.item, input)
+var DbBooks = model.Books{
+	Item: []model.Book{
+		{Id: 0, Title: "Clean Code", Author: "Robert C.Martin", Page: 750, Release: 1990},
+		{Id: 1, Title: "Introduction to Algorithms", Author: "Thomas H. Cormen", Page: 500, Release: 1993},
+		{Id: 2, Title: "The Clean Coder", Author: "Robert C.MArtin", Page: 450, Release: 1997},
+	},
 }
