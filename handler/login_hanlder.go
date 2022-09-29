4a83d50be569127aa531fc34f50838eb26f1b1b9
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-29 21:24:54 +0700

    [JWT] Add login handler

--- handler/login_hanlder.go
+++ handler/login_hanlder.go
@@ -4,32 +4,40 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"time"
 
+	"github.com/MarioTiara/Go-API-Gin/authentication"
+	"github.com/MarioTiara/Go-API-Gin/user"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var jwtKey = []byte("secret_key")
+// var users = map[string]string{
+// 	"user1": "password1",
+// 	"user2": "password2",
+// }
 
-var users = map[string]string{
-	"user1": "password1",
-	"user2": "password2",
+// type Crediantials struct {
+// 	Username string `json:"username"`
+// 	Password string `json:"password"`
+// }
+
+// type Claims struct {
+// 	Username string `json:"username"`
+// 	jwt.StandardClaims
+// }
+
+type loginHandler struct {
+	userService user.Service
 }
 
-type Crediantials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+func NewLoginHandler(userService user.Service) *loginHandler {
+	return &loginHandler{userService}
 }
 
-type Claims struct {
-	Username string `json:"username"`
-	jwt.StandardClaims
-}
-
-func LoginHandler(c *gin.Context) {
-	var crediantials Crediantials
-
+func (handler *loginHandler) LoginHandler(c *gin.Context) {
+	var crediantials authentication.Crediantials
 	err := json.NewDecoder(c.Request.Body).Decode(&crediantials)
 	fmt.Println(crediantials)
 	if err != nil {
@@ -37,15 +45,19 @@ func LoginHandler(c *gin.Context) {
 		return
 	}
 
-	expectedPassword, ok := users[crediantials.Username]
-	if !ok || expectedPassword != crediantials.Password {
+	user, err := handler.userService.FindByName(crediantials.Username)
+	if err != nil {
+		c.Writer.WriteString("username is not found")
+		return
+	}
+
+	if user.Password != crediantials.Password {
 		c.Writer.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
 	expirationTime := time.Now().Add(time.Minute * 5)
-
-	claims := &Claims{
+	claims := &authentication.Claims{
 		Username: crediantials.Username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationTime.Unix(),
@@ -53,12 +65,11 @@ func LoginHandler(c *gin.Context) {
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
+	tokenString, err := token.SignedString(authentication.JwtKey)
 	if err != nil {
 		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	c.JSON(http.StatusOK, gin.H{
 		"status": "OK",
 		"token":  tokenString,
